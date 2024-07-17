4bb2ef55e34117b73774ab21a7934fa0962e06fe
Jake Neal <hello@jakeneal.co.uk>
2024-07-17 23:55:34 +0100

    :art: Remove use of 'prop' and add prop defaults

--- src/components/ui/AppButton/AppButton.vue
+++ src/components/ui/AppButton/AppButton.vue
@@ -2,22 +2,24 @@
     <button
         :class="[
             'app-button',
-            props.variant === 'primary'
+            variant === 'primary'
                 ? 'text-white rounded-md bg-aqua px-8 py-4 hover:bg-aqua/70 focus:bg-aqua/70'
                 : 'text-aqua',
         ]"
     >
-        <span v-if="props.icon" class="pr-1" data-testid="icon">{{ props.icon }}</span>
-        {{ props.text }}
+        <span v-if="icon" class="pr-1" data-testid="icon">{{ icon }}</span>
+        {{ text }}
     </button>
 </template>
 
 <script lang="ts" setup>
 export type AppButtonProps = {
-    variant?: string;
+    variant: string;
     text: string;
     icon?: string;
 };
 
-const props = defineProps<AppButtonProps>();
+const props = withDefaults(defineProps<AppButtonProps>(), {
+    variant: () => 'link',
+});
 </script>
