147ada3637cacc17d35946e2c86469e0b5994210
Jake Neal <hello@jakeneal.co.uk>
2024-07-17 22:01:16 +0100

    :fire: Removed test directive

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,6 @@ import App from './App.vue';
 
 const app = createApp(App);
 
-app.directive('test', {
-    created(el, { arg }) {
-        if (typeof process === 'undefined' || !process.env.VITEST) {
-            return;
-        }
-
-        el.setAttribute('data-testid', arg);
-    },
-});
-
 app.use(createPinia());
 
 app.mount('#app');
